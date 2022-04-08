5b79f60feea64ac8aa39d63956205f70f59311cf
ved <ved.43d@gmail.com>
2022-04-08 10:12:57 +0300

    form handler

---
+++ js/script.js
@@ -0,0 +1,117 @@
+"user strict"
+
+document.addEventListener('DOMContentLoaded', function(){
+	const form = document.querySelector("#form")
+
+	form.addEventListener('submit', formSend)
+	
+	async function formSend(e){
+		e.preventDefault();
+
+		let error = formValidate(form);
+
+		let formData = new FormData(form)
+		formData.append('image', formFile.files[0])
+
+		
+		if(error === 0){
+			form.classList.add('_sending')
+			let response = await fetch('sendmail.php',{
+				method: 'POST',
+				body: formData
+			});
+			if(response.ok){
+				let result = await response.json();
+				alert(result.message);
+				console.log(result)
+				formPreview.innerHTML = '';
+				form.reset();
+				form.classList.remove('_sending')
+			}else{
+				alert('error')
+				form.classList.remove('_sending')
+			}	
+		}else{
+			alert('заполните обязательные поля')
+		}
+	}
+
+	function formValidate(form){
+		let error = 0;
+		let formReq = document.querySelectorAll('[data-req]');
+
+		for(let i = 0; i < formReq.length; i++){
+			const input = formReq[i];
+
+			formRemoveError(input)
+
+			if(input.dataset.req == 'email'){
+				if(emailTeset(input)){
+					formAddError(input)
+					error ++;
+				}
+			}else if(input.getAttribute("type") === "checkbox" && input.checked === false){
+				formAddError(input)
+				error ++;
+			}else{
+				if(input.value === ''){
+					formAddError(input)
+					error ++;
+				}
+			}
+		}
+
+		return error;
+	}
+
+	function formAddError(input){
+		input.parentElement.setAttribute("data-error","");
+		input.setAttribute("data-error","");
+	}
+
+	function formRemoveError(input){
+		input.parentElement.removeAttribute("data-error");
+		input.removeAttribute("data-error");
+	}
+
+	function emailTeset(input){
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	}
+
+	const formFile = document.getElementById('formFile');
+	const formPreview = document.getElementById('formPreview');
+
+	formFile.addEventListener('change', ()=>{
+		uploadFile(formFile.files[0]);
+	});
+
+	function uploadFile(file){
+		if(!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)){
+			alert('Разрешены только изображения');
+			return
+		}
+
+		if(file.size > 2 * 1024 * 1024){
+			alert('Файл должен быть меньше 2 мб');
+			return
+		}
+		
+		let reader = new FileReader();
+
+		reader.onload = function(e){
+			formPreview.innerHTML = `<img src='${e.target.result}' alt="photo">`;		
+		};
+
+		reader.onerror = function(e){
+			alert('error');
+		}
+
+		reader.readAsDataURL(file);
+	}
+
+});
+
+
+
+
+
---
+++ sendmail.php
@@ -0,0 +1,93 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->isHTML(true);
+
+$mail->setFrom('ved.43d@mail.com', 'professional');
+$mail->addAddress('user@gmail.com');
+$mail->Subject = 'Привет это твой суп  ер программист';
+
+$hand = 'Right';
+if($_POST['hand'] == 'left'){
+	$hand = 'Left';
+}
+
+$body = '<h1>Title this Message</h1>';
+
+if(trim(!empty($_POST['name']))){
+	$body.= '<p><stron>Имя</strong> '.$_POST['name'].'</p>';
+}
+
+if(trim(!empty($_POST['email']))){
+	$body.= '<p><stron>E-mail</strong>'.$_POST['email'].'</p>';
+}
+
+if(trim(!empty($_POST['hand']))){
+	$body.= '<p><stron>Рука</strong>'.$hand.'</p>';
+}
+
+if(trim(!empty($_POST['age']))){
+	$body.= '<p><stron>Возраст</strong>'.$_POST['age'].'</p>';
+}
+
+if(trim(!empty($_POST['message']))){
+	$body.= '<p><stron>Сообщение</strong>'.$_POST['message'].'</p>';
+}
+
+
+if(!empty($_FILES['image']['tmp_name'])){
+	$filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
+
+	if(copy($_FILES['image']['tmp_name'], $filePath)){
+		$fileAttach = $filePath;
+		$body.='<p><strong>Фото в приложении</strong></p>';
+		$mail->addAttachment($fileAttach);
+	}
+}
+
+$mail->Body = $body;
+
+
+if(!$mail->send()){
+	$message = 'Error';
+}else{
+	$message = 'Данные отравлены!';
+}
+
+$response = ['message' => $message];
+
+header('Content-type: application/json');
+echo json_encode($response);
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
